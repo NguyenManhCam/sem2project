8410fc22c00e860287131347b2abc7c8f86abbc6
namphd00769 <namphd00769@fpt.edu.vn>
2019-03-22 11:49:57 +0700

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import HomeScreen from "./containers/HomeScreen";
 import Logins from "./components/Logins";
+import StudentList from "./components/StudentList";
 
 class App extends Component {
-  state = {};
+	state = {};
 
-  onLogin = (username, password) => {
-    this.setState({ username: username, password: password });
-  };
+	onLogin = (username, password) => {
+		this.setState({ username: username, password: password });
+	};
 
-  render() {
-    return (
-      <div>
-        {/* <HomeScreen /> */}
-        {/* {this.state.username=="admin" && this.state.password=="123456" ? <HomeScreen /> :
+	render() {
+		return (
+			<div>
+				{/* <HomeScreen /> */}
+				{/* {this.state.username=="admin" && this.state.password=="123456" ? <HomeScreen /> :
         {/* <Logins /> */}
-        <Logins />
-      </div>
-    );
-  }
+				<Logins />
+				{/* <StudentList /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/components/Logins.js
+++ client/src/components/Logins.js
@@ -4,85 +4,76 @@ import bg from "../images/test1.png";
 import axios from "../axios";
 
 class Logins extends Component {
-  state = {}
-  componentDidMount() {}
+	state = {};
+	componentDidMount() {}
 
-  handleSubmit = async e => {
-    e.preventDefault();
-    this.props.form.validateFields(async (err, values) => {
-      if (!err) {
-        console.log("Received values of form: ", values);
-        let data = "";
-        try {
-          data = await axios.post("/api/Users/login", values);
-        } catch (error) {
-          console.log(error);
-          this.setState({ noti: "Ten tai khoan hoac password khong dung" })
-        }
-        console.log(data);
-      }
-    });
-  };
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div style={{}}>
-        <Form
-          style={{ margin: "0 auto" }}
-          onSubmit={this.handleSubmit}
-          className="login-form"
-        >
-          <Form.Item>
-            {getFieldDecorator("email", {
-              rules: [
-                { required: true, message: "Please input your username!" }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Username"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("password", {
-              rules: [
-                { required: true, message: "Please input your Password!" },
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                type="password"
-                placeholder="Password"
-              />
-            )}
-          </Form.Item>
-          {this.state.noti ? <p style={{ color: "red" }}>{this.state.noti}</p> : ""}
-          <Form.Item>
-            {getFieldDecorator("remember", {
-              valuePropName: "checked",
-              initialValue: true
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              Log in
-            </Button>
-            Or <a href="">register now!</a>
-          </Form.Item>
-        </Form>
-      </div>
-    );
-  }
+	handleSubmit = async e => {
+		e.preventDefault();
+		this.props.form.validateFields(async (err, values) => {
+			if (!err) {
+				console.log("Received values of form: ", values);
+				let data = "";
+				try {
+					data = await axios.post("/api/Users/login", values);
+				} catch (error) {
+					console.log(error);
+					this.setState({ noti: "Ten tai khoan hoac password khong dung" });
+				}
+				console.log(data);
+			}
+		});
+		// let params = { limit: 5 };
+		// let a = await axios.get(`/api/Students?filter=${JSON.stringify(params)}`);
+		// console.log(a);
+	};
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<div style={{}}>
+				<Form
+					style={{ margin: "0 auto" }}
+					onSubmit={this.handleSubmit}
+					className="login-form"
+				>
+					<Form.Item>
+						{getFieldDecorator("email", {
+							rules: [{ required: true, message: "Please input your username!" }]
+						})(
+							<Input
+								prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+								placeholder="Username"
+							/>
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator("password", {
+							rules: [{ required: true, message: "Please input your Password!" }]
+						})(
+							<Input
+								prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+								type="password"
+								placeholder="Password"
+							/>
+						)}
+					</Form.Item>
+					{this.state.noti ? <p style={{ color: "red" }}>{this.state.noti}</p> : ""}
+					<Form.Item>
+						{getFieldDecorator("remember", {
+							valuePropName: "checked",
+							initialValue: true
+						})(<Checkbox>Remember me</Checkbox>)}
+						<a className="login-form-forgot" href="">
+							Forgot password
+						</a>
+						<Button type="primary" htmlType="submit" className="login-form-button">
+							Log in
+						</Button>
+						Or <a href="">register now!</a>
+					</Form.Item>
+				</Form>
+			</div>
+		);
+	}
 }
 
 export default Form.create()(Logins);
